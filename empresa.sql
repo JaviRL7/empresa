f62f08131684b14b50422bb1aed53d6934dc8b20
JaviRL7 <Jrlsanlucar11@gmail.com>
2022-10-14 17:29:05 +0200

    11/10/22

--- empresa.sql
+++ empresa.sql
@@ -1,17 +1,16 @@
 DROP TABLE IF EXISTS departamentos CASCADE;
 
-CREATE TABLE departamentos(
-    id BIGSERIAL PRIMARY KEY
-   , codigo NUMERIC(2) NOT NULL UNIQUE
-   , denominacion varchar(255) NOT NULL
-
+CREATE TABLE departamentos (
+    id           bigserial    PRIMARY KEY,
+    codigo       numeric(2)   NOT NULL UNIQUE,
+    denominacion varchar(255) NOT NULL
 );
 
--- Fixtures: 
+-- Fixtures:
 
 INSERT INTO departamentos (codigo, denominacion)
-    VALUES (10 ,'Informatica'),
-            (20, 'administrativo'),
-            (30, 'prevencion'),
-            (40,'laboratorio'),
-            (50, 'automocion')
+    VALUES (10, 'Informática'),
+           (20, 'Administrativo'),
+           (30, 'Prevención'),
+           (40, 'Laboratorio'),
+           (50, 'Automoción');
--- index.php
+++ index.php
@@ -1,17 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Departamentos</title>
 </head>
 <body>
-    <?php 
-    $pdo = new PDO('pgsql:host=localhost;dbname=empresa','empresa','empresa');
-    $sent = $pdo->query('SELECT * FROM departamentos');
-    $filas = $sent->fetchAll();
-    print_r($filas);
+    <?php
+    $desde_codigo = (isset($_GET['desde_codigo'])) ? trim($_GET['desde_codigo']) : null;
+    $hasta_codigo = (isset($_GET['hasta_codigo'])) ? trim($_GET['hasta_codigo']) : null;
     ?>
+    <div>
+        <form action="" method="get">
+            <fieldset>
+                <legend>Criterios de búsqueda</legend>
+                <p>
+                    <label>
+                        Desde código:
+                        <input type="text" name="desde_codigo" size="8" value="<?= $desde_codigo ?>">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Hasta código:
+                        <input type="text" name="hasta_codigo" size="8" value="<?= $hasta_codigo ?>">
+                    </label>
+                </p>
+                <button type="submit">Buscar</button>
+            </fieldset>
+        </form>
+    </div>
+    <?php
+    $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
+    $pdo->beginTransaction();
+    $sent = $pdo->query('LOCK TABLE departamentos IN SHARE MODE');
+    $sent = $pdo->prepare('SELECT COUNT(*)
+                             FROM departamentos
+                            WHERE codigo BETWEEN :desde_codigo AND :hasta_codigo');
+    $sent->execute([
+        ':desde_codigo' => $desde_codigo,
+        ':hasta_codigo' => $hasta_codigo,
+    ]);
+    $total = $sent->fetchColumn();
+    $sent = $pdo->prepare('SELECT *
+                             FROM departamentos
+                            WHERE codigo BETWEEN :desde_codigo AND :hasta_codigo
+                         ORDER BY codigo');
+    $sent->execute([
+        ':desde_codigo' => $desde_codigo,
+        ':hasta_codigo' => $hasta_codigo,
+    ]);
+    $pdo->commit();
+    ?>
+    <br>
+    <div>
+        <table style="margin: auto" border="1">
+            <thead>
+                <th>Código</th>
+                <th>Denominación</th>
+            </thead>
+            <tbody>
+                <?php foreach ($sent as $fila): ?>
+                    <tr>
+                        <td><?= $fila['codigo'] ?></td>
+                        <td><?= $fila['denominacion'] ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+        <p>Número total de filas: <?= $total ?></p>
+    </div>
 </body>
 </html>
